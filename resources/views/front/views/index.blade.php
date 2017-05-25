7a254a7afc11818f2571d8a1021ac60ef9a147fa
rockey <1570677190@qq.com>
2017-05-25 16:44:19 +0800

    nothing

--- resources/views/front/views/index.blade.php
+++ resources/views/front/views/index.blade.php
@@ -9,48 +9,9 @@
     <link rel="stylesheet" href="/css/bootstrap.css"/>
     <script src="/js/zepto.min.js" type="text/javascript"></script>
     <script src="/js/vipspa.js" type="text/javascript"></script>
-    <!-- <script type="text/javascript">
-        $(function () {
-            $('.weui-label').css({
-                'text-align': 'center'
-            });
-            $('#login').load('login.html');//初始load的界面
-            $('body').load('footer-tabs/footer-tabs.html');
-
-            //路由配置文件
-            $(function(){
-                vipspa.start({
-                    view: '#ui-view',
-                    router: {
-                        'me': {
-                            templateUrl: 'views/me.blade.php',
-                            controller: 'js/app/me.js'
-                        },
-                        'message': {
-                            templateUrl: 'views/message.blade.php',
-                            controller: 'js/app/message.js'
-                        },
-                        'search': {
-                            templateUrl: 'views/search.blade.php',
-                            controller: 'js/app/search.js'
-                        },
-                        'weixin': {
-                            templateUrl: 'views/weixin.blade.php',
-                            controller: 'js/app/weixin.js'
-                        },
-                        'defaults': 'me' //默认路由
-                    },
-                    errorTemplateId: '#error'  //可选的错误模板，用来处理加载html模块异常时展示错误内容
-                });
-
-            });
-        })
-    </script> -->
+    
 </head>
 <body>
-    <!-- 暂且隐藏 先编写路由
-        <div id="login"></div>
-    -->
     <div class="weui-tabbar">
     <a href="#weixin" class="weui-tabbar__item weui-bar__item_on">
                     <span style="display: inline-block;position: relative;">
