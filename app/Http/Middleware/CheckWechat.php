621d601882434c404bccad0eba185389f30eea39
zxj95121 <986773071@qq.com>
2017-05-29 14:57:25 +0800

    中间件

--- app/Http/Middleware/CheckWechat.php
+++ app/Http/Middleware/CheckWechat.php
@@ -17,7 +17,7 @@ class CheckWechat
      */
     public function handle($request, Closure $next)
     {
-        if(isset($_SESSION['openid']))
+        if(Session::get('openid'))
             return $next($request);
         else if (Request::is('front/error_403')) {
             return $next($request);
