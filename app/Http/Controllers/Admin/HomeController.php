76c0d119f1dcb989a9fcaf16b40dbde3bd31bdab
zxj95121 <986773071@qq.com>
2017-05-29 23:30:30 +0800

    URL写错了

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -45,7 +45,7 @@ class HomeController extends Controller
     			$qrcodeInfo['id'] = $flight->id;
     			$qrcodeInfo['qrcode'] = url('/admin/images/login_qrcode/'.$flight->scan_url);
 
-                QrCode::format('png')->size(200)->generate(url('/admin/scanConfirmOauth?id='.$qrcodeInfo['id']), public_path($path));
+                QrCode::format('png')->size(200)->generate(url('/admin/scanConfirm/oauth?id='.$qrcodeInfo['id']), public_path($path));
 
     		} else {
 				$qrcodeInfo['id'] = $timeOver[0]->id;
