42ea16663b04af25e1a04f3c2bbe5c54b6135c8f
zxj95121 <986773071@qq.com>
2017-06-02 17:48:57 +0800

    7

--- app/Http/Controllers/Front/LoginController.php
+++ app/Http/Controllers/Front/LoginController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Front;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Http\Libraries\Wechat;
 
 use App\Http\Controllers\Wechat\OauthController;
 
