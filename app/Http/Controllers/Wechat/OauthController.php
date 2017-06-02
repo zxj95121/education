c93e9cbfe09fcd46f7af0ec4ef15b8233ba5dd2f
zxj95121 <986773071@qq.com>
2017-06-02 21:28:33 +0800

    36

--- app/Http/Controllers/Wechat/OauthController.php
+++ app/Http/Controllers/Wechat/OauthController.php
@@ -58,7 +58,7 @@ class OauthController extends Controller
     		$openid = $data['openid'];
     		Session::put('openid', $openid);
     		Session::put('oauth_access_token', $access_token);
-            Session::save();
+            // Session::save();
     		return redirect($redirect_url);
     	} else if(array_key_exists('openid', $data)) {
     		//静默授权进来的
