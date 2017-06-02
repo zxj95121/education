b91b1ddc7c70e6dcde3d877b5980037e4aa02d34
zxj95121 <986773071@qq.com>
2017-06-02 21:00:37 +0800

    25

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -31,8 +31,8 @@ class Kernel extends HttpKernel
         'web' => [
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
-            \App\Http\Middleware\CheckWechat::class,
+            // \Illuminate\Session\Middleware\StartSession::class,
+            // \App\Http\Middleware\CheckWechat::class,
             \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
