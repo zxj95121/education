d48f3c4a761e631d66b69ae956b57fa88cbd03fc
zxj95121 <986773071@qq.com>
2017-06-02 17:28:13 +0800

    4

--- app/Http/Middleware/CheckWechat.php
+++ app/Http/Middleware/CheckWechat.php
@@ -19,7 +19,7 @@ class CheckWechat
     {
         var_dump($request->session()->get('openid'));
         echo '<br />';
-        var_dump($request->all()->toArray());
+        var_dump($request->all());
         if($request->session()->get('openid'))
             return $next($request);
         else if (Request::is('front/error_403')) {
