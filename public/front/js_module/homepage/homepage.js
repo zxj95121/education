49afef5e3a02b27216eeca70e6858d27d42047f9
zxj95121 <986773071@qq.com>
2017-06-03 16:06:49 +0800

    图片问题

--- public/front/js_module/homepage/homepage.js
+++ public/front/js_module/homepage/homepage.js
@@ -5,7 +5,11 @@ $(function(){
 		$('#all_bottom .weui-tabbar__item').each(function(){
 			var sforDiv = $(this).attr('for');
 			$('#'+sforDiv).css('display', 'none');
+			var src = $(this).find('img').attr('src');
+			src = src.replace('_fill','');
+			$(this).find('img').attr('src', src);
 		})
 		$('#'+forDiv).css('display', 'block');
+		$(this).find('img').attr('src', $(this).find('img').attr('src').replace('.png', '_fill.png'));
 	})
 })
