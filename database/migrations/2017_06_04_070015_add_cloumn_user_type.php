b5583a2415879067a1ebb639a6d316624cad1538
zxj95121 <986773071@qq.com>
2017-06-04 15:02:44 +0800

    migration

---
+++ database/migrations/2017_06_04_070015_add_cloumn_user_type.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCloumnUserType extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_type', function (Blueprint $table) {
+            $table->integer('uid')->after('type')->comment('指向该用户的ID');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $table->dropColumn('uid');
+    }
+}
