47954a63eaffd721787719c4a2a54173ae1a4bda
zxj95121 <986773071@qq.com>
2017-06-19 17:19:02 +0800

    35

--- resources/views/front/views/user_info/user_info_teacher_add.blade.php
+++ resources/views/front/views/user_info/user_info_teacher_add.blade.php
@@ -564,7 +564,101 @@ $signPackage = $jssdk->GetSignPackage();
 	<script type="text/javascript">
 		$(function(){
 			/*价格*/
-
+			picker1({
+		    	id: 'myMoneyPicker', 
+		    	action: 'moneyPicker',
+		    	content: [
+					[{
+			            name: '50元',
+			            value: 50
+			        }, {
+			            name: '60元',
+			            value: 60,
+			            checked: true
+			        },{
+			            name: '70元',
+			            value: 70
+			        },{
+			            name: '80元',
+			            value: 80
+			        },{
+			            name: '90元',
+			            value: 90
+			        },{
+			            name: '100元',
+			            value: 100
+			        },{
+			            name: '110元',
+			            value: 110
+			        },{
+			            name: '120元',
+			            value: 120
+			        },{
+			            name: '130元',
+			            value: 130
+			        },{
+			            name: '140元',
+			            value: 140
+			        },{
+			            name: '150元',
+			            value: 150
+			        },{
+			            name: '160元',
+			            value: 160
+			        },{
+			            name: '170元',
+			            value: 170
+			        },{
+			            name: '180元',
+			            value: 180
+			        },{
+			            name: '190元',
+			            value: 190
+			        },{
+			            name: '200元',
+			            value: 200
+			        },{
+			            name: '210元',
+			            value: 210
+			        },{
+			            name: '220元',
+			            value: 220
+			        },{
+			            name: '230元',
+			            value: 230
+			        },{
+			            name: '240元',
+			            value: 240
+			        },{
+			            name: '250元',
+			            value: 250
+			        },{
+			            name: '260元',
+			            value: 260
+			        },{
+			            name: '270元',
+			            value: 270
+			        },{
+			            name: '280元',
+			            value: 280
+			        },{
+			            name: '290元',
+			            value: 290
+			        },{
+			            name: '300元',
+			            value: 300
+			        }],	 [{
+			            name: '60分钟',
+			            value: 60
+			        }, {
+			            name: '90分钟',
+			            value: 90
+			        }]
+		    	],
+		    	select: function(result){
+		    		$('#moneyPicker span').html(result[0] + '元 / ' + result[1] +'分钟');
+		    	}
+		    });
 		});
 	</script>
 	<script type="text/javascript">
@@ -650,6 +744,46 @@ $signPackage = $jssdk->GetSignPackage();
 				cropper.zoomOut();
 			})
 		});
+
+		function picker1(data){
+			selfPicker['size1'] = selfPicker.size;
+    		selfPicker.length =1;
+    		selfPicker.current =1;
+    		selfPicker['size'+(selfPicker.current)].action = data.action;/*进行绑定的这个名称*/
+    		selfPicker['size'+(selfPicker.current)].id = data.id;
+    		selfPicker['size'+(selfPicker.current)].content = data.content;
+    		selfPicker['size'+(selfPicker.current)].select = data.select;
+
+    		selfPicker.arr[data.action] = selfPicker.current;
+    		$('#'+data.action).click(function(){
+    			console.log(selfPicker);
+    			$('#pickerBigDiv').css('display', 'block');
+    			selfPicker.current = selfPicker.arr[$(this).attr('id')];
+    			$('#'+selfPicker['size'+(selfPicker.current)].id).css('display','block');
+    		})
+
+    		picker_init(selfPicker['size'+(selfPicker.current)].id, data.content);//初始化插件
+		}
+
+		function picker2(data){
+			selfPicker['size2'] = selfPicker.size;
+    		selfPicker.length =2;
+    		selfPicker.current =2;
+    		selfPicker['size'+(selfPicker.current)].action = data.action;/*进行绑定的这个名称*/
+    		selfPicker['size'+(selfPicker.current)].id = data.id;
+    		selfPicker['size'+(selfPicker.current)].content = data.content;
+    		selfPicker['size'+(selfPicker.current)].select = data.select;
+
+    		selfPicker.arr[data.action] = selfPicker.current;
+    		$('#'+data.action).click(function(){
+    			console.log(selfPicker);
+    			$('#pickerBigDiv').css('display', 'block');
+    			selfPicker.current = selfPicker.arr[$(this).attr('id')];
+    			$('#'+selfPicker['size'+(selfPicker.current)].id).css('display','block');
+    		})
+
+    		picker_init(selfPicker['size'+(selfPicker.current)].id, data.content);//初始化插件
+		}
 	</script>
 </body>
 </html>
