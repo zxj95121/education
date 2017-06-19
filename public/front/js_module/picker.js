8cf74a735b6bae3eea4494ad895042d2406fc2f8
zxj95121 <986773071@qq.com>
2017-06-19 15:12:18 +0800

    25

--- public/front/js_module/picker.js
+++ public/front/js_module/picker.js
@@ -10,7 +10,7 @@ $(function () {
     		selfPicker['size'+(selfPicker.length)].select = data.select;
     		selfPicker.arr[data.action] = selfPicker.length;
     		$('#'+data.action).click(function(){
-    			console.log(selfPicker.)
+    			console.log(selfPicker.arr);
     			$('#pickerBigDiv').css('display', 'block');
     			selfPicker.current = selfPicker.arr[$(this).attr('id')];
     			$('#'+selfPicker['size'+(selfPicker.current)].id).css('display','block');
