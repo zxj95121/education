ad49c7944bcf47694fe46045d0ec863603f00303
zxj95121 <986773071@qq.com>
2017-06-19 10:27:54 +0800

    还原

--- public/front/js_module/picker.js
+++ public/front/js_module/picker.js
@@ -51,7 +51,7 @@ $(function () {
     		$(this).animate({'marginTop': '105px'}, 500);
     	}
     	var allLength = (selfPicker.content[$(this).index('#'+selfPicker.id+' .colPicker')].length-4)*35;
-    	if (allLength > 0 && (marginTop+allLength < 0|| marginTop < (105+allLength))) {
+    	if (allLength > 0 && (marginTop+allLength < 0)) {
     		$(this).animate({'marginTop': '-'+allLength+'px'}, 500);
     	}
 
