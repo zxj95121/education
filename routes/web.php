e4c721735528068eb9f1fa2c4bedc0d51d53e3bd
zxj95121 <986773071@qq.com>
2017-06-22 16:00:17 +0800

    少了个封号

--- routes/web.php
+++ routes/web.php
@@ -138,7 +138,7 @@ Route::group(['prefix' => 'front','namespace' => 'Front','middleware' => ['wecha
     $router->get('/user_info_parent','UserInfoController@parent');
     $router->get('/user_info_teacher','UserInfoController@teacher');
     /*信息跳转*/
-    $router->get('/selectTeacherType','UserInfoController@selectTeacherType')
+    $router->get('/selectTeacherType','UserInfoController@selectTeacherType');
         /*teacher信息保存*/
     $router->post('/tsave_headimg', 'UserInfoController@t_headimg');
     $router->post('/tsave_nickname', 'UserInfoController@t_nickname');
