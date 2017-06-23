8bb174d6d821139d2d95c2b4a653785c7ede5fe5
zxj95121 <986773071@qq.com>
2017-06-23 20:27:14 +0800

    更改样式，jquery-mobile

--- resources/views/front/views/user_info/user_teacher_redirect.blade.php
+++ resources/views/front/views/user_info/user_teacher_redirect.blade.php
@@ -1,41 +1,31 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>请选择您的身份</title>
-	<meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="/js/weui/weui.min.css" />
-	<link rel="stylesheet" type="text/css" href="/admin/css/bootstrap.min.css" />
+<title></title>
+<link rel="stylesheet" href="/js/jquery-mobile/jquery.mobile-1.4.5.css">
+<script type="text/javascript" src="/admin/js/jquery-1.11.1.min.js"></script>
+<script src="/js/jquery-mobile/jquery.mobile-1.4.5.js"></script>
 </head>
 <body>
-	<div class="container" style="max-width: 500px;margin:0 auto;padding: 0px;position: relative;text-align: center;">
-		<a href="javascript:;" class="weui-btn weui-btn-mini weui-btn_default" type="1">大学生教师</a>
-		<a type="2" href="javascript:;" class="weui-btn weui-btn-mini weui-btn_default">职业教师</a>
-		
-		<br>
-		<a href="javascript:;" id="confirm" class="weui-btn weui-btn_primary">确认身份</a>
+
+	<div data-role="page">
+  		<div data-role="header">
+  			<h1>选择菜单</h1>
+  		</div>
+
+	 	<div data-role="content" data-theme="b">
+	    	<form method="post" action="demoform.asp">
+	      		<fieldset data-role="fieldcontain">
+	        		<label for="day">选择身份</label>
+	        		<select name="day" id="day">
+	         			<option value="sat">大学生教师</option>
+	         			<option value="sun">职业教师</option>
+	        		</select>
+	      		</fieldset>
+	      		<input type="submit" data-inline="true" value="提交">
+	    	</form>
+	  	</div>
 	</div>
 
-	<script type="text/javascript" src="/admin/js/jquery-1.11.1.min.js"></script>
-	<script type="text/javascript">
-		$(function(){
-			$('.weui-btn-mini').click(function(){
-				var index = $(this).index();
-				if(index == 1){
-					$(this).prev().removeClass('weui-btn_primary').addClass('weui-btn_default');
-				} else {
-					$(this).next().removeClass('weui-btn_primary').addClass('weui-btn_default');
-				}
-
-				$(this).removeClass('weui-btn_default').addClass('weui-btn_primary');
-			});
-
-			$('#confirm').click(function(){
-				if ($('.weui-btn_default').length == 1) {
-					var type = $('.container .weui-btn_primary:eq(0)').attr('type');
-					window.location.href = '/front/selectTeacherType?type='+type;
-				}
-			})
-		})
-	</script>
 </body>
 </html>
