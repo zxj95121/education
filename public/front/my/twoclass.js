a84253801804d876f3bd413dd8ccf1defb8ce3b4
nagnahz <15075138688@163.com>
2017-07-02 02:32:39 +0800

    阿斯蒂芬

--- public/front/my/twoclass.js
+++ public/front/my/twoclass.js
@@ -1,16 +1,16 @@
 $(document).on("click",".class1",function(){
 	var pid = $(this).attr('pid');
-	$('#twoclass').load('/front/twoClass?pid='.pid);
+	$('#twoclass').load('/front/twoClass?pid='+pid);
 });
 $(document).on('click','.class2',function(){
 	var pid = $(this).attr('pid');
-	$('#twoclass').load('/front/twoClasstwo?pid='.pid);
+	$('#twoclass').load('/front/twoClasstwo?pid='+pid);
 })
 $(document).on('click','.class3',function(){
 	var pid = $(this).attr('pid');
-	$('#twoclass').load('/front/twoClassthree?pid='.pid);
+	$('#twoclass').load('/front/twoClassthree?pid='+pid);
 })
 $(document).on('click','.class4',function(){
 	var pid = $(this).attr('pid');
-	$('#twoclass').load('/front/twoClassfour?pid='.pid);
+	$('#twoclass').load('/front/twoClassfour?pid='+pid);
 })
