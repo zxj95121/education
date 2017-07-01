be56c01ca5353be02b0d493f3c4b86e1d254bae5
nagnahz <15075138688@163.com>
2017-07-02 02:18:42 +0800

    js

--- public/front/my/twoclass.js
+++ public/front/my/twoclass.js
@@ -1,17 +1,17 @@
-
-	$(document).on('click','.class1',function(){
-		var pid = $(this).attr('pid');
-		$('#eclass').load('/front/twoClass?pid='.pid);
-	});
-	$(document).on('click','.class2',function(){
-		var pid = $(this).attr('pid');
-		$('#eclass').load('/front/twoClasstwo?pid='.pid);
-	})
-	$(document).on('click','.class3',function(){
-		var pid = $(this).attr('pid');
-		$('#eclass').load('/front/twoClassthree?pid='.pid);
-	})
-	$(document).on('click','.class4',function(){
-		var pid = $(this).attr('pid');
-		$('#eclass').load('/front/twoClassfour?pid='.pid);
-	})
+$(document).on("click",".class1",function(){
+	console.log(123);
+	//var pid = $(this).attr('pid');
+	//$('#eclass').load('/front/twoClass?pid='.pid);
+});
+$(document).on('click','.class2',function(){
+	var pid = $(this).attr('pid');
+	$('#eclass').load('/front/twoClasstwo?pid='.pid);
+})
+$(document).on('click','.class3',function(){
+	var pid = $(this).attr('pid');
+	$('#eclass').load('/front/twoClassthree?pid='.pid);
+})
+$(document).on('click','.class4',function(){
+	var pid = $(this).attr('pid');
+	$('#eclass').load('/front/twoClassfour?pid='.pid);
+})
