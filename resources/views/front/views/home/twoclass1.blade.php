52d9bcef4f33f02269f37698cf460433e30d7e71
nagnahz <15075138688@163.com>
2017-07-01 21:10:38 +0800

    双师class1

--- resources/views/front/views/home/twoclass1.blade.php
+++ resources/views/front/views/home/twoclass1.blade.php
@@ -1,8 +1,9 @@
 @foreach($res as $value)
+<div class="weui-cells__title">双师class1</div>
 <div class="weui-cells">
 	<a class="weui-cell weui-cell_access" href="/twoClasstwo?fid={{$value->id}}">
 	    <div class="weui-cell__bd">
-	        <p>{$value->name}</p>
+	        <p>{{$value->name}}</p>
 	    <iframe id="tmp_downloadhelper_iframe" style="display: none;"></iframe></div>
 	    <div class="weui-cell__ft">
 	    </div>
