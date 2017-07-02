871fc4a0852329c50d6ce269ff02477954796d0e
zxj95121 <986773071@qq.com>
2017-07-02 22:59:36 +0800

    fdsa

--- resources/views/front/views/user_info/user_info_teacher_add.blade.php
+++ resources/views/front/views/user_info/user_info_teacher_add.blade.php
@@ -379,11 +379,11 @@ $signPackage = $jssdk->GetSignPackage();
                             </div>
                             <div class="pbody">
                             	@foreach($value as $v)
-                            		@if(strpos($userDetail->hobby, $v['id']) !== false)
-                                		<button type="button" class="btn btn-success" cid="{{$v['id']}}">{{$v['name']}}</button>
+                            		@php if(strpos($userDetail->hobby, $v['id']) !== false) 
+                                		echo '<button type="button" class="btn btn-success" cid="'.$v['id'].'">'.$v['name'].'</button>';
                                 	@else
-                                		<button type="button" class="btn btn-info" cid="{{$v['id']}}">{{$v['name']}}</button>
-                                	@endif
+                                		echo '<button type="button" class="btn btn-info" cid="'.$v['id'].'">'.$v['name'].'</button>';
+                                	@endphp
                                 @endforeach
                             </div> 
                         </div>
