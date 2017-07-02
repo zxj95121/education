22915358d4e5a67ab43363d42eab31f8f2ba0c5b
zxj95121 <986773071@qq.com>
2017-07-02 16:37:28 +0800

    妈卖批

--- resources/views/front/views/user_info/user_info_teacher_add.blade.php
+++ resources/views/front/views/user_info/user_info_teacher_add.blade.php
@@ -516,10 +516,9 @@ $signPackage = $jssdk->GetSignPackage();
 	        	$('#community .col:eq(2)').html('');
 
 	        	for (var i in communityInfo[num]['next'][number]['next']){
+	        		$('#community .col:eq(2)').append('<div class="row row_community cThree" number="'+i+'" cid = "'+communityInfo[num]['next'][number]['next'][i].id+'">'+communityInfo[num]['next'][number]['next'][i].name+'</div>');
 	        		if ($.inArray(communityInfo[num]['next'][number]['next'][i].id, address)) {
-	        			$('#community .col:eq(2)').append('<div class="row row_community cThree communityActive" number="'+i+'" cid = "'+communityInfo[num]['next'][number]['next'][i].id+'">'+communityInfo[num]['next'][number]['next'][i].name+'</div>');
-	        		} else {
-	        			$('#community .col:eq(2)').append('<div class="row row_community cThree" number="'+i+'" cid = "'+communityInfo[num]['next'][number]['next'][i].id+'">'+communityInfo[num]['next'][number]['next'][i].name+'</div>');
+	        			$('#community .col:eq(2)').find('.cThree:last').addClass('communityActive');
 	        		}
 	        	}
 	        })
