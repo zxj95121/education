0b8518c3b24ea73383f1508aa352677612b99e6c
nagnahz <15075138688@163.com>
2017-07-02 23:29:26 +0800

    阿阿

--- resources/views/front/views/home/twoclass.blade.php
+++ resources/views/front/views/home/twoclass.blade.php
@@ -33,14 +33,17 @@
 		@endforeach
 	@else
 		@foreach($res as $value)
-			<div class="weui-cell">
-				<a class="{{$class}}" pid="{{$value->id}}">
-                	<div class="weui-cell__bd">
-                    	<p>{{$value->name}}</p>
-                	</div>
-                </a>
-               		<div class="weui-cell__ft"><a>购买</a></div>
-            </div>
+			<div  class="weui-cells" style="margin:0">
+				<a class="weui-cell {{$class}}" pid="{{$value->id}}">
+				    <div class="weui-cell__bd">
+				        <p style="margin-bottom:0px">{{$value->name}}</p>
+				    	<iframe id="tmp_downloadhelper_iframe" style="display: none;"></iframe>
+				    </div>
+				</a>
+				<a>
+					<div class="weui-cell__ft">购买</div>
+				</a>
+			</div>
         @endforeach		
 	@endif
 
