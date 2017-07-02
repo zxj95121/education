1e7540e55acc39583c4b01c19608e8a17fcdcff4
nagnahz <15075138688@163.com>
2017-07-02 13:13:43 +0800

    calss

--- public/front/my/twoclass.js
+++ public/front/my/twoclass.js
@@ -1,16 +1,12 @@
-$(document).on("click",".class1",function(){
-	var pid = $(this).attr('pid');
-	$('#twoclass').load('/front/twoClass?pid='+pid);
-});
-$(document).on('click','.class2',function(){
+$(document).on('click','.class1',function(){
 	var pid = $(this).attr('pid');
 	$('#twoclass').load('/front/twoClasstwo?pid='+pid);
 })
-$(document).on('click','.class3',function(){
+$(document).on('click','.class2',function(){
 	var pid = $(this).attr('pid');
 	$('#twoclass').load('/front/twoClassthree?pid='+pid);
 })
-$(document).on('click','.class4',function(){
+$(document).on('click','.class3',function(){
 	var pid = $(this).attr('pid');
 	$('#twoclass').load('/front/twoClassfour?pid='+pid);
 })
