98c39d8b333406e6a98d5cbc0e66e0dfdeef5423
nagnahz <15075138688@163.com>
2017-07-02 15:48:00 +0800

    阿阿

--- public/front/my/twoclass.js
+++ public/front/my/twoclass.js
@@ -4,20 +4,17 @@ $(document).on('click','.class1',function(){
 })
 $(document).on('click','.class2',function(){
 	var pid = $(this).attr('pid');
-	$('#houtui').attr('pid1',pid);
+	$('#houtui').prop('pid1',pid);
 	$('#twoclass').load('/front/twoClassthree?pid='+pid);
 })
 $(document).on('click','.class3',function(){
 	var pid = $(this).attr('pid');
-	$('#houtui').attr('pid2',pid);
+	$('#houtui').prop('pid2',pid);
 	$('#twoclass').load('/front/twoClassfour?pid='+pid);
 })
 $(document).on('click','#houtui',function(){
 	var twoclass = $(this).attr('fenlei');
 	switch(twoclass){
-		case'class1': 
-			var pid = 0;
-			break;
 		case'class2':
 			var pid = 0;
 			break;
