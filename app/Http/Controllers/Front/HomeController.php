a4064f7c05a53d0963c8aba11b9675808088483a
zxj95121 <986773071@qq.com>
2017-07-03 10:38:57 +0800

    牛皮

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -6,11 +6,13 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 use App\Http\Controllers\Wechat\OauthController;
+use Session;
 
 class HomeController extends Controller
 {
     public function home()
     {
+    	$openid = Session::get('openid');
     	return view('front.views.home.homepage');
     }
 
