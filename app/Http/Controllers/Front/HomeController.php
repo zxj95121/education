e14d9292de8a6ceec3a5e4353575f0f1d191162f
nagnahz <15075138688@163.com>
2017-07-04 20:57:19 +0800

    测试

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -30,8 +30,8 @@ class HomeController extends Controller
 		$res['userType'] = UserType::where('openid', $openid)
 			->select('type', 'uid')
 			->get();
-		dump($res['userType'][0]);
-		dump($res['userType'] == false);
+		dd($res['userType']);
+		dd($res['userType'][0]);
 		switch($res['userType'][0]->type){
 			case '1':
 				$res['data'] = AdminInfo::where('openid',$openid)->get();
