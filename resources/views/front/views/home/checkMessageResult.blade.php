53ad76d500db1b9266f57cc078d45612fd5adeda
zxj95121 <986773071@qq.com>
2017-07-04 23:42:57 +0800

    23日

--- resources/views/front/views/home/checkMessageResult.blade.php
+++ resources/views/front/views/home/checkMessageResult.blade.php
@@ -21,9 +21,24 @@
         @endforeach
     </div>
 <!--     <div class="weui-form-preview__ft"> -->
-        <a href="/front/user_info_parent" style="width: 95%;margin: 0 auto;" class="weui-btn weui-btn_warn">现在去完善信息</a>
+        <a href="/front/user_info_parent" style="width: 90%;margin: 0 auto;" class="weui-btn weui-btn_warn">现在去完善信息</a>
     <!-- </div> -->
 </div>
 
 @else
+    <div id="dialogs">
+        <!--BEGIN dialog1-->
+        <div class="js_dialog" id="iosDialog1" style="opacity: 1;">
+            <div class="weui-mask"></div>
+            <div class="weui-dialog">
+                <div class="weui-dialog__hd"><strong class="weui-dialog__title">订单详情</strong></div>
+                <div class="weui-dialog__bd">弹窗内容，告知当前状态、信息和解决方法，描述文字尽量控制在三行内</div>
+                <div class="weui-dialog__ft">
+                    <a href="javascript:;" class="weui-dialog__btn weui-dialog__btn_default">取消订单</a>
+                    <a href="javascript:;" class="weui-dialog__btn weui-dialog__btn_primary">确认订单</a>
+                </div>
+            </div>
+        </div>
+        <!--END dialog1-->
+    </div>
 @endif
