ca724639a364860d0cc1836f2277173ede675a54
zxj95121 <986773071@qq.com>
2017-07-04 23:14:32 +0800

    页面展示

--- resources/views/front/views/home/checkMessageResult.blade.php
+++ resources/views/front/views/home/checkMessageResult.blade.php
@@ -1,11 +1,11 @@
 @if($result)
 <div class="weui-form-preview" style="">
-    <div class="weui-form-preview__hd">
+<!--     <div class="weui-form-preview__hd">
         <div class="weui-form-preview__item">
             <label class="weui-form-preview__label">出错提示</label>
             <em class="weui-form-preview__value">关闭</em>
         </div>
-    </div>
+    </div> -->
     <div class="weui-form-preview__bd">
         @foreach($result as $value)
         <div class="weui-form-preview__item">
@@ -14,9 +14,9 @@
         </div>
         @endforeach
     </div>
-    <div class="weui-form-preview__ft">
+<!--     <div class="weui-form-preview__ft"> -->
         <a href="javascript:;" class="weui-btn weui-btn_warn">现在去完善信息</a>
-    </div>
+    <!-- </div> -->
 </div>
 
 @else
