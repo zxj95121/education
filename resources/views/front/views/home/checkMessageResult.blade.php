d9b5dcf4856f2e9ae4a4e0f00d8ef72089240504
zxj95121 <986773071@qq.com>
2017-07-04 23:53:56 +0800

    23

--- resources/views/front/views/home/checkMessageResult.blade.php
+++ resources/views/front/views/home/checkMessageResult.blade.php
@@ -32,7 +32,28 @@
             <div class="weui-mask"></div>
             <div class="weui-dialog">
                 <div class="weui-dialog__hd"><strong class="weui-dialog__title">订单详情</strong></div>
-                <div class="weui-dialog__bd">弹窗内容，告知当前状态、信息和解决方法，描述文字尽量控制在三行内</div>
+                <div class="weui-dialog__bd">
+                    <div class="weui-cells">
+                        <div class="weui-cell">
+                            <div class="weui-cell__bd">
+                                <p>标题文字</p>
+                            </div>
+                            <div class="weui-cell__ft">说明文字</div>
+                        </div>
+                        <div class="weui-cell">
+                            <div class="weui-cell__bd">
+                                <p>标题文字</p>
+                            </div>
+                            <div class="weui-cell__ft">说明文字</div>
+                        </div>
+                        <div class="weui-cell">
+                            <div class="weui-cell__bd">
+                                <p>标题文字</p>
+                            </div>
+                            <div class="weui-cell__ft">说明文字</div>
+                        </div>
+                    </div>
+                </div>
                 <div class="weui-dialog__ft">
                     <a href="javascript:;" class="weui-dialog__btn weui-dialog__btn_default">取消订单</a>
                     <a href="javascript:;" class="weui-dialog__btn weui-dialog__btn_primary">确认订单</a>
