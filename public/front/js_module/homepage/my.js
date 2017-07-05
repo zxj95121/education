5fcedb64eb9eb132d5932e214594333bf2d956cc
zxj95121 <986773071@qq.com>
2017-07-06 00:25:01 +0800

    fbgfd

--- public/front/js_module/homepage/my.js
+++ public/front/js_module/homepage/my.js
@@ -4,10 +4,10 @@ $(function(){
 		$(this).css('height', width);
 	})
 
-	console.log($('.jiange').width());
+	// console.log($('.jiange')[0].offsetWidth)
 	/*间隔高度*/
-	// $('.jiange').css('width', parseInt($('.jiange:eq(0)').width())+parseInt($('#my').css('paddingLeft'))*2+'px');
-	// $('.jiange').css({'position':'relative', 'right': $('#my').css('paddingLeft')});
+	$('.jiange').css('width', parseInt($('.jiange')[0].offsetWidth)+parseInt($('#my').css('paddingLeft'))*2+'px');
+	$('.jiange').css({'position':'relative', 'right': $('#my').css('paddingLeft')});
 
 	/*bottom*/
 	$('#my_option').css('marginBottom', $('#all_bottom').css('height'));
