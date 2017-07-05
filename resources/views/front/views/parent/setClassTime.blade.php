103a9926b82a64849772e4e509619c9e0e8c0b77
zxj95121 <986773071@qq.com>
2017-07-05 21:01:55 +0800

    csrf

--- resources/views/front/views/parent/setClassTime.blade.php
+++ resources/views/front/views/parent/setClassTime.blade.php
@@ -3,6 +3,7 @@
 <head>
 	<title>加辰教育</title>
 	<meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0">
+	<meta name="csrf-token" content="{{csrf_token()}}">
 	<link rel="stylesheet" type="text/css" href="/css/sm.min.css">
 	<link rel="stylesheet" type="text/css" href="/css/weui.css">
 	<style type="text/css">
