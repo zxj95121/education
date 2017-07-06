f59802887a3715ee5bddc08e47048ea8439cc8c4
zxj95121 <986773071@qq.com>
2017-07-06 10:50:08 +0800

    fw

--- resources/views/front/views/user_info/user_info_parent_add.blade.php
+++ resources/views/front/views/user_info/user_info_parent_add.blade.php
@@ -586,8 +586,7 @@ $signPackage = $jssdk->GetSignPackage();
 		    $('.done_ok1').click(function(){
 		    	var index = $(this).index('.done_ok1');
 		    	var value = $(this).parents('.page_set').find('input').val();
-		    	console.log(index);
-		    	var arr = new Array(/^.{1,10}$/, /^.{1,2}$/, /^[\u4e00-\u9fa5]{1,4}$/, /[\u4e00-\u9fa5]{1,10}/);
+		    	var arr = new Array(/[\u4e00-\u9fa5]{1,10}/,/^[\u4e00-\u9fa5]{1,4}$/, /^.{1,2}$/, /^.{1,10}$/);
 		    	if (!arr[index].test(value) && !(index == 2 && value == '')) {
 		    		layer.open({
 					    content: '长度或格式不正确'
