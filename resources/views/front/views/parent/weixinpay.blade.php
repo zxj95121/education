949df06c79cc5e8b176bf79acecac769b66aed45
nagnahz <15075138688@163.com>
2017-07-09 04:19:57 +0800

    测试

--- resources/views/front/views/parent/weixinpay.blade.php
+++ resources/views/front/views/parent/weixinpay.blade.php
@@ -36,7 +36,6 @@ $input->SetOpenid($openId);//用户标识
 $order = WxPayApi::unifiedOrder($input);
 echo '<font color="#f00"><b>统一下单支付单信息</b></font><br/>';
 printf_info($order);
-die;
 $jsApiParameters = $tools->GetJsApiParameters($order);
 
 //获取共享收货地址js函数参数
