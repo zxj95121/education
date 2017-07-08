e54767d811d0d14ebad926cf81c5fd8e31bbcf27
nagnahz <15075138688@163.com>
2017-07-09 04:15:33 +0800

    测试

--- app/Http/Controllers/HtmlController.php
+++ app/Http/Controllers/HtmlController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 class HtmlController extends Controller
 {
     public function index(Request $request)
