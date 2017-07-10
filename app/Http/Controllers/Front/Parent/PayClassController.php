19247820524ce8fcefd3131b60a9752efa14cf6b
zxj95121 <986773071@qq.com>
2017-07-11 00:42:25 +0800

    no message

--- app/Http/Controllers/Front/Parent/PayClassController.php
+++ app/Http/Controllers/Front/Parent/PayClassController.php
@@ -25,7 +25,7 @@ class PayClassController extends Controller
 		$uid = $this->getUid($openid);
 		/*新订单*/
 		$tid = $request->input('id');
-		dd($request->all());
+		dd($tid);
 		/*查取价格*/
 		$res = EclassPriceController::getUnitPrice($tid);
 		$count = $res['count'];
