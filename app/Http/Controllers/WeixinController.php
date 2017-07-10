608d4a06a356074ba72b1eef994cfc5f30a6d6e4
nagnahz <15075138688@163.com>
2017-07-10 20:12:38 +0800

    测试

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -4,14 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-
+use Illuminate\Support\Facades\Storage;
 class WeixinController extends Controller
 {
     public function notify(Request $request)
     {
-    	$date = $request->all();
+     	$date = $request->all();
+    	Storage::put('a.txt', $date); 
+    	$postStr = file_get_contents('php://input');
     		DB::table('ceshi')->insert([
-    				'text'=>$date
+    				'text'=>$postStr
     		]);
     }
 
