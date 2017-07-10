2d399e1781adfa9e2a723bcdeaed4dbed7b4b742
nagnahz <15075138688@163.com>
2017-07-10 22:07:57 +0800

    测试

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -3,18 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Storage;
+use App\Models\EclassOrder;
+use App\Models\Bill;
 class WeixinController extends Controller
 {
     public function notify(Request $request)
     {
-     	$date = $request->all();
-    	Storage::put('a.txt', $date); 
     	$postStr = file_get_contents('php://input');
-    		DB::table('ceshi')->insert([
-    				'text'=>$postStr
-    		]);
+    	if (!empty($postStr)){
+    		$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+    		if($postObj->result_code == 'SUCCESS'){
+    			$order = EclassOrder::where('order_no',$postObj->out_trade_no)->first();
+    			$order->pay_status = 1;
+    			$order->save();
+    			$bill = Bill::where('transaction_id',$postObj->transaction_id)->get();
+    			if(count($bill) == 0){
+    				$bill = new Bill();
+    				$bill->transaction_id = $postObj->transaction_id;
+    				$bill->oid = $order->id;
+    				$bill->openid = $postObj->appid;
+    				$bill->save();
+    			}
+    		}
+    	}
+    	return true;
     }
 
 }
