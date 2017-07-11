341db852e1842c66ccb7db5997d9e549ff3611eb
zxj95121 <986773071@qq.com>
2017-07-12 06:30:48 +0800

    v安师大

--- resources/views/front/views/parent/myClassOrder.blade.php
+++ resources/views/front/views/parent/myClassOrder.blade.php
@@ -13,13 +13,28 @@
 	      		<span class="icon icon-left"></span>
 	      		返回
 	    	</a>
+	    	<a class="icon icon-refresh pull-right"></a>
 	    	<h1 class="title">历史订单</h1>
 	  	</header>
-	  	<nav class="bar bar-tab">
-	    	
-	  	</nav>
 	  	<div class="content">
-			
+			<div class="content-block-title">Facebook卡片</div>
+			  	<div class="card facebook-card">
+			    	<div class="card-header no-border">
+			      		<div class="facebook-avatar">
+			      			<img src="http://gqianniu.alicdn.com/bao/uploaded/i4//tfscom/i3/TB10LfcHFXXXXXKXpXXXXXXXXXX_!!0-item_pic.jpg_250x250q60.jpg" width="34" height="34">
+			      		</div>
+			      		<div class="facebook-name">夜萧</div>
+			      		<div class="facebook-date">星期一 3:47pm</div>
+			    	</div>
+			    	<div class="card-content">
+			    		<img src="http://gqianniu.alicdn.com/bao/uploaded/i4//tfscom/i3/TB10LfcHFXXXXXKXpXXXXXXXXXX_!!0-item_pic.jpg_250x250q60.jpg" width="100%">
+			    	</div>
+		    		<div class="card-footer no-border">
+				      	<a href="#" class="link">赞</a>
+				      	<a href="#" class="link">评论</a>
+				      	<a href="#" class="link">分享</a>
+				    </div>
+		  		</div>
 	  	</div>
 	</div>
 
