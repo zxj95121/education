e799c688196f4dc05ef8628e49e27bf8ace4d37c
zxj95121 <986773071@qq.com>
2017-07-12 22:04:52 +0800

    no message

--- app/Http/Controllers/HtmlController.php
+++ app/Http/Controllers/HtmlController.php
@@ -6,26 +6,13 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\TemplateController;
 class HtmlController extends Controller
 {
-    // public function index(Request $request)
-    // {
-    // 	// $res = explode('/', $_SERVER['REQUEST_URI'])[2];
-    // 	// echo $res;
-    // 	$res = $request->input('res');
-    // 	$views = $this->$res();
-    // 	return view($views);
-    // }
-    public function index(){
-        $access_token = 'kIHWDyEjSBDeGhOnGk_vKGWoAoqlh0CW7E4j0qdmX0vdcyTy6b6j6IKcsD7M_pSv9YqvOk_k5nEecaHvFmxRMaLgqnH7mL8jxh2a4LDVYbMgWPQGB3BinVp9KU9M8GhbWWRiAGAZZU';
-        $openid = 'oild4wHv3pTetKtuaI28ylHO3fIM';
-        // $bill = new Bill();
-        // $bill->oid = $order->id;
-        // $bill->save();
-        // $parentObj = ParentInfo::find($uid);
-        // $name = EclassPriceController::getName($order->tid, 2);
-        // $firstName = EclassPriceController::getName($order->tid, 0);
-        $name = '张安安';
-        $firstName = 'zxj';
-        Template::send($openid,'关于双师Class订单支付成功的通知',$firstName,$name,'15','201515202','订单支付成功，请耐心等待管理员审核');
+    public function index(Request $request)
+    {
+    	// $res = explode('/', $_SERVER['REQUEST_URI'])[2];
+    	// echo $res;
+    	$res = $request->input('res');
+    	$views = $this->$res();
+    	return view($views);
     }
 
     private function register(){
