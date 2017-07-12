77640d545736078aea9ffa4737107d3a8acdd8ca
zxj95121 <986773071@qq.com>
2017-07-12 17:40:26 +0800

    use App\Models\UserType;

--- app/Http/Controllers/Front/Parent/MyClassOrderController.php
+++ app/Http/Controllers/Front/Parent/MyClassOrderController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 use App\Models\EclassOrder;
+use App\Models\UserType;
 use App\Http\Controllers\EclassPriceController;
 use Session;
 
