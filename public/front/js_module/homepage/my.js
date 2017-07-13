cbbbca527dc8a7fafd6796eaecd72333efb95f2c
nagnahz <15075138688@163.com>
2017-07-13 17:49:39 +0800

    测试

--- public/front/js_module/homepage/my.js
+++ public/front/js_module/homepage/my.js
@@ -15,11 +15,6 @@ $(function(){
 
 	/*添加孩子点击*/
 	$('#addChild').click(function(){
-/*		layer.open({
-		    content: '该功能正在开发中...'
-		    ,skin: 'msg'
-		    ,time: 2 //2秒后自动关闭
-		});*/ 
 		window.location.href = "/front/parent/addChild";
 	})
 })
