bd81b4a3415d81de63d77cc2d67cafd872ab030f
zxj95121 <986773071@qq.com>
2017-07-14 00:56:17 +0800

    发生大

--- public/front/my/twoclass.js
+++ public/front/my/twoclass.js
@@ -28,7 +28,7 @@ $(document).on('click','.class3',function(){
 			success: function(data){
 				if (data.errcode == 0) {
 					layer.close(loadIndex);
-					var str = '<div class="weui-skin_android" id="childsheet" style="opacity: 0;"> <div class="weui-mask child-weui-mask"></div> <div class="weui-actionsheet"> <div class="weui-actionsheet__menu"> ';
+					var str = '<div class="weui-skin_android" id="childsheet" style="opacity: 0;display: none;"> <div class="weui-mask child-weui-mask"></div> <div class="weui-actionsheet"> <div class="weui-actionsheet__menu"> ';
 					for (var i in data.child) {
 						str += '<div class="weui-actionsheet__cell child_Cell" cid=""><input type="radio" name="child" value="'+data.child[i].id+'" />'+data.child[i].name+'</div>';
 					}
