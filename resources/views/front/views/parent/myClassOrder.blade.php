d1a2ea6afab05f71c784af9c92e36f8b69bb35d2
nagnahz <15075138688@163.com>
2017-07-14 21:59:51 +0800

    测试

--- resources/views/front/views/parent/myClassOrder.blade.php
+++ resources/views/front/views/parent/myClassOrder.blade.php
@@ -18,7 +18,7 @@
 	  	</header>
 	  	<div class="content">
 		  	<div class="buttons-tab">
-			    <a href="#tab1" class="tab-link action button">待付款</a>
+			    <a href="#tab1" class="tab-link active button">待付款</a>
 			    <a href="#tab2" class="tab-link button">待审核</a>
 			    <a href="#tab3" class="tab-link button">授课中</a>
 			    <a href="#tab4" class="tab-link button">已完成</a>
@@ -157,8 +157,10 @@
 		}
 		var ids = GetQueryString('action');
 		if(ids){
+			console.log(2);
 			$('.buttons-tab').find('a').eq(parseInt(ids)-1).trigger('click');
 		}else{
+			console.log(1);
 			$('.buttons-tab').find('a').eq(0).trigger('click');
 		}
 	</script>
