4fecdf5c38d5fbaf64669cebf5c80e83cb547b1c
zxj95121 <986773071@qq.com>
2017-07-15 21:16:08 +0800

    no message

--- app/Http/Controllers/TemplateController.php
+++ app/Http/Controllers/TemplateController.php
@@ -13,16 +13,16 @@ class TemplateController extends Controller
     	$res = '{
 				"touser":"'.$openid.'",
 				"template_id":"Aes29u_WuVTbyxSZPHqB5g3gzjEsx7Pj_9CSqJXLB70",
-				"url": "http://'.$_SERVER["SERVER_NAME"].'/parent/myClassOrder/oauth",
+				"url": "http://'.$_SERVER["SERVER_NAME"].'/front/parent/myClassOrder/oauth",
 				"topcolor":"#FF0000",
     			"data":{
-    				"first":{"value": "'.$title.'","color":"#173177"},
+    				"first":{"value": "'.$title.'"},
     				"keyword1":{"value": "'.$name.'","color":"#173177"},
     				"keyword2":{"value": "'.$type.'","color":"#173177"},
     				"keyword3":{"value": "'.$price.'","color":"#173177"},
     				"keyword4":{"value": "'.$time.'","color":"#173177"},
     				"keyword5":{"value": "'.$nickname.'","color":"#173177"},
-    				"remark":{"value": "'.$remark.'","color":"#173177"}
+    				"remark":{"value": "'.$remark.'"}
     				}
     			}';
     	Wechat::curl('https://api.weixin.qq.com/cgi-bin/message/template/send?access_token='.$access_token['access_token'],$res);
