efd6a68a6145550c4068a848b0ad33d72ad09792
zxj95121 <986773071@qq.com>
2017-07-22 10:18:50 +0800

    no message

--- resources/views/front/views/home/twoclass.blade.php
+++ resources/views/front/views/home/twoclass.blade.php
@@ -62,7 +62,7 @@
         	<div id="myCart" style="height: 40px;background: #F90000;width: 100%;">
                 <div class="col-xs-12" style="width:100%;height:100%;text-align: center;">
                     <span style="vertical-align: middle;line-height: 40px;color:#7C7C7C;">我的购物车</span>
-                    <span class="weui-badge" style="margin-left: 5px;background: #F90000;color:#FFF;">8</span>
+                    <span class="weui-badge" style="margin-left: 5px;background: #FFF;color:#F90000;">8</span>
                 </div>
             </div>	
 	@endif
