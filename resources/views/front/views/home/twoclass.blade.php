c8602483c4b03862c1459fbed5ed04630c7a72be
zxj95121 <986773071@qq.com>
2017-07-22 19:08:49 +0800

    no message

--- resources/views/front/views/home/twoclass.blade.php
+++ resources/views/front/views/home/twoclass.blade.php
@@ -79,8 +79,8 @@
 
             <div id="orderdetail" style="position: fixed;z-index: 99;width: 100%;">
             	<div class="cartblock">
-            		<div class="cartheader" style="width:100%;background: #D8E0F7;">
-            			<h1 class="page__title">英语自然拼读</h1>
+            		<div class="cartheader" style="width:100%;background: #D8E0F7;padding:6px 10px;">
+            			<p><h2>英语自然拼读</h2></p>
             		</div>
             		<div class="cartcontent" style="width: 100%;background: #FFF;">
             			fsdfbr	<br>fsdafb <br>fsadfsdb <br>大幅度br	<br>防守打法
