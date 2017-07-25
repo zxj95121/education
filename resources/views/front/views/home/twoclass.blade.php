bec7f669a56d7997a053c9c7dc64003010a6651f
zxj95121 <986773071@qq.com>
2017-07-26 07:05:00 +0800

    no message

--- resources/views/front/views/home/twoclass.blade.php
+++ resources/views/front/views/home/twoclass.blade.php
@@ -31,7 +31,7 @@
 <div id="twoclass">
 	<div class="weui-cell weui-cell_access twoclassHead" style="height:40px;background:#22AAE8;color:#FFF;">
 	    @if($class != 'class1')
-	     	<div><div class="glyphicon glyphicon-menu-left" id="houtui" fenlei="{{$class}}"></div></div>
+	     	<div><div class="glyphicon glyphicon-menu-left" id="houtui" fenlei="{{$class}}">返回</div></div>
 	    @endif
 	    <div class="weui-flex__item"><div class="placeholder" style="text-align:center; font-size:16px">请选择课程</div></div>
 	</div>
