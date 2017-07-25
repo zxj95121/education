ebc1f414fe7cc5eab299a55b007bccecc5102d4b
zxj95121 <986773071@qq.com>
2017-07-25 11:51:04 +0800

    no message

--- app/Models/classPackageOrder.php
+++ app/Models/classPackageOrder.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class classPackageOrder extends Model
+class ClassPackageOrder extends Model
 {
     protected $table = 'class_package_order';
 }
