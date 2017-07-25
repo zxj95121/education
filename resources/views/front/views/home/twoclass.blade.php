885e73a6a7538ba145149251bdee3f7574bfdf62
zxj95121 <986773071@qq.com>
2017-07-26 07:12:58 +0800

    no message

--- resources/views/front/views/home/twoclass.blade.php
+++ resources/views/front/views/home/twoclass.blade.php
@@ -31,7 +31,7 @@
 <div id="twoclass">
 	<div class="weui-cell weui-cell_access twoclassHead" style="height:40px;background:#22AAE8;color:#FFF;padding: 0px 15px;">
 	    @if($class != 'class1')
-	     	<div style="height: 80%;position: absolute;top: 10%;"><div class="glyphicon glyphicon-menu-left" id="houtui" fenlei="{{$class}}" style="line-height: 32px;">返回</div></div>
+	     	<div style="height: 80%;position: absolute;top: 10%;"><div class="glyphicon glyphicon-menu-left" id="houtui" fenlei="{{$class}}" style="line-height: 32px;"><span>返回</span></div></div>
 	    @endif
 	    <div class="weui-flex__item"><div class="placeholder" style="text-align:center; font-size:16px">请选择课程</div></div>
 	</div>
