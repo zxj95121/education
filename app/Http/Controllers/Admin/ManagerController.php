aa57277b2287984bc6ac47eecc5f7d610dbe7cab
nagnahz <15075138688@163.com>
2017-07-25 22:45:07 +0800

    测试

--- app/Http/Controllers/Admin/ManagerController.php
+++ app/Http/Controllers/Admin/ManagerController.php
@@ -120,11 +120,16 @@ class ManagerController extends Controller
     	for($i = 0; $i < count($res); $i++){
     		
     		$arr = explode('-',$res[$i]->money);
-    		if($arr[1] == 1){
-    			$res[$i]->money = $arr[0].'/月';
+    		if(count($arr) > 0){
+    			if($arr[1] == 1){
+    				$res[$i]->money = $arr[0].'/月';
+    			}else{
+    				$res[$i]->money = $arr[0].'/'.$arr[1].'分钟';
+    			}
     		}else{
-    			$res[$i]->money = $arr[0].'/'.$arr[1].'分钟';
+    			$res[$i]->money = '暂未填写';
     		}
+
             if( !empty( $res[$i]->hobby ) ){
                 $arr1 = explode('-',$res[$i]->hobby);
 
