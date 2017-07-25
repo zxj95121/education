f1c1995319a8646fd76eacb8b0f2b43cca62fe92
nagnahz <15075138688@163.com>
2017-07-25 10:51:51 +0800

    分享1

--- app/Http/Controllers/Front/ShareController.php
+++ app/Http/Controllers/Front/ShareController.php
@@ -14,28 +14,18 @@ class ShareController extends Controller
 	public function index(Request $request)
 	{
 		$openid = Session::get('openid');
-		$access_token = Wechat::get_access_token();
 		
-		/*获取用户个人详细信息*/
-		$url = 'https://api.weixin.qq.com/sns/userinfo?access_token='.
-				$access_token['access_token'].'&openid='.
-				$openid.'&lang=zh_CN';
-		$userinfo = Wechat::curl($url);
-		dump($userinfo);
-		$subscribe = $userinfo['subscribe'];
-
-		$id = $request->input('id');
-		$type = $request->input('type');
-		if($id){
-			$newuser = NewUser::find($id);
+		if(Session::get('share')){
+			$id = Session::get('share')['id'];
 			$usershare = new UserShare();
-			$usershare->openid = $newuser->openid;
-			$usershare->subscribe = $subscribe;
+			$usershare->openid = $openid;
+			$usershare->subscribe = 0;
 			$usershare->save();
+			Session::forget('share');
 		}
 		$newuser = NewUser::where('openid',$openid)->get()[0];
 		if($newuser->id){
-			$news = array("Title" =>"加辰教育", "Description"=>"加辰教育123", "PicUrl" =>'http://'.$_SERVER['SERVER_NAME'].'/admin/img/index.png', "Url" =>"http://".$_SERVER['SERVER_NAME']."/front/share/oauth?type=".$newuser->type."&&id=".$newuser->id);
+			$news = array("Title" =>"加辰教育", "Description"=>"加辰教育123", "PicUrl" =>'http://'.$_SERVER['SERVER_NAME'].'/admin/img/index.png', "Url" =>"http://".$_SERVER['SERVER_NAME']."/front/share/oauth?id=".$newuser->id);
 		}else{
 			$news = array("Title" =>"加辰教育", "Description"=>"加辰教育123", "PicUrl" =>'http://'.$_SERVER['SERVER_NAME'].'/admin/img/index.png', "Url" =>"http://".$_SERVER['SERVER_NAME']."/front/share/oauth");
 		}
@@ -44,6 +34,10 @@ class ShareController extends Controller
 	/*oauth*/
 	public function oauth(Request $request)
 	{
+		if($request->input('id')){
+			$share['id'] = $request->input('id');
+			Session::put('share',$share);
+		}
 		return redirect(OauthController::getUrl(7, 0));
 	}
 	
