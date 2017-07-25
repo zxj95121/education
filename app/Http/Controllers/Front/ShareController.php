0153cb449017a4a05ee8fab173db492f5d0805e9
nagnahz <15075138688@163.com>
2017-07-25 16:19:47 +0800

    分享13

--- app/Http/Controllers/Front/ShareController.php
+++ app/Http/Controllers/Front/ShareController.php
@@ -16,7 +16,7 @@ class ShareController extends Controller
 		$openid = Session::get('openid');
 		$access_token = Wechat::get_access_token();
 		/*获取用户个人详细信息*/
-		$url = 'https://api.weixin.qq.com/sns/userinfo?access_token='.
+		$url = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token='.
 				$access_token['access_token'].'&openid='.
 				$openid.'&lang=zh_CN';
 		$userinfo = Wechat::curl($url);
