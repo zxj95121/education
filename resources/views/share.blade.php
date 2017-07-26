e4dc2d24a36be2518a64f97745959e429c85fd75
zxj95121 <986773071@qq.com>
2017-07-26 15:22:24 +0800

    no message

--- resources/views/share.blade.php
+++ resources/views/share.blade.php
@@ -7,15 +7,15 @@ $signPackage = $jssdk->GetSignPackage();
 <html>
 	<head>
 		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<!-- <meta http-equiv="X-UA-Compatible" content="IE=edge"> -->
 		<title>加辰教育定制</title>
 		<meta name="viewport" content="initial-scale=1, maximum-scale=1">
-		<link rel="shortcut icon" href="/favicon.ico">
+<!-- 		<link rel="shortcut icon" href="/favicon.ico">
 		<meta name="apple-mobile-web-app-capable" content="yes">
-		<meta name="apple-mobile-web-app-status-bar-style" content="black">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black"> -->
 
-		<link rel="stylesheet" href="//g.alicdn.com/msui/sm/0.6.2/css/sm.min.css">
-		<link rel="stylesheet" href="//g.alicdn.com/msui/sm/0.6.2/css/sm-extend.min.css">
+<!-- 		<link rel="stylesheet" href="//g.alicdn.com/msui/sm/0.6.2/css/sm.min.css">
+		<link rel="stylesheet" href="//g.alicdn.com/msui/sm/0.6.2/css/sm-extend.min.css"> -->
 	</head>
 	<body>
 		<section data-role="outer" label="Powered by 135editor.com" style="font-family:微软雅黑;font-size:16px;">
@@ -197,10 +197,10 @@ $signPackage = $jssdk->GetSignPackage();
     </section>
 </section>
 
-		<script type='text/javascript' src='//g.alicdn.com/sj/lib/zepto/zepto.min.js' charset='utf-8'></script>
+		<!-- <script type='text/javascript' src='//g.alicdn.com/sj/lib/zepto/zepto.min.js' charset='utf-8'></script>
 		<script type='text/javascript' src='//g.alicdn.com/msui/sm/0.6.2/js/sm.min.js' charset='utf-8'></script>
-		<script type='text/javascript' src='//g.alicdn.com/msui/sm/0.6.2/js/sm-extend.min.js' charset='utf-8'></script>
-		<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+		<script type='text/javascript' src='//g.alicdn.com/msui/sm/0.6.2/js/sm-extend.min.js' charset='utf-8'></script> -->
+		<script src="http://res.wx.qq.com/open/js/jweixin-1.2.0.js"></script>
 		<script>
 		    wx.config({
 		        debug: false,
