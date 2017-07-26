2de8d084f34d7d36e8e445451735b9318b597505
zxj95121 <986773071@qq.com>
2017-07-26 15:23:54 +0800

    no message

--- resources/views/share.blade.php
+++ resources/views/share.blade.php
@@ -9,7 +9,7 @@ $signPackage = $jssdk->GetSignPackage();
 		<meta charset="utf-8">
 		<!-- <meta http-equiv="X-UA-Compatible" content="IE=edge"> -->
 		<title>加辰教育定制</title>
-		<meta name="viewport" content="initial-scale=1, maximum-scale=1">
+		<meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0">
 <!-- 		<link rel="shortcut icon" href="/favicon.ico">
 		<meta name="apple-mobile-web-app-capable" content="yes">
 		<meta name="apple-mobile-web-app-status-bar-style" content="black"> -->
