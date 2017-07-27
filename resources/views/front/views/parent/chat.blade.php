068e6a268a620c2a0a5b73497796d0309f48b78a
zxj95121 <986773071@qq.com>
2017-07-27 22:51:11 +0800

    no message

--- resources/views/front/views/parent/chat.blade.php
+++ resources/views/front/views/parent/chat.blade.php
@@ -149,13 +149,12 @@
 			$('#chat-messages .message').each(function(index){
 				// var height = $(this).child()
 				// console.log(index);
-				if ($(this).children('span').has('.chatData')) {
+				console.log($(this).find('.chatData').length);
 					var height = $(this).find('.chatData')[0].offsetHeight;
 					if (height > 20) {
 						$('#chat-messages .message').eq(index).css('padding-bottom', (height+18) +'px');
 					}
 					$('#chat-messages .message').eq(index).css('height', height+'px');
-				}
 				// console.log(height);
 				// console.log($(this).find('.chatData')[0].offsetWidth);
 			})
