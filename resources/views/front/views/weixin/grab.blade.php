36e8e5aeedbcf84259afd3a9e7c7d7d26fab49c7
nagnahz <15075138688@163.com>
2017-07-29 14:31:04 +0800

    测试啊

--- resources/views/front/views/weixin/grab.blade.php
+++ resources/views/front/views/weixin/grab.blade.php
@@ -6,6 +6,22 @@
 		<link rel="stylesheet" type="text/css" href="/admin/css/bootstrap.css">
 	</head>
 	<body>
+		<table>
+			<tr>
+				<th>活动名称</th>
+				<th>开始时间</th>
+			</tr>
+			<tr>
+				<td>{{$res[0]->name}}</td>
+				<td>{{$res[0]->start_time}}</td>
+			</tr>
+		</table>
+		<table id="t2">
+			<tr><th>中奖名单</th></tr>
+			@foreach($lucky as $value)
+			<tr><td>{{$value->nickname}}</td></tr>
+			@endforeach
+		</table>
 		<button id="join" val="{{$res->id}}">参加活动</button>
 		<script type="text/javascript" src="/admin/js/jquery-1.11.1.min.js"></script>
 		<script>
@@ -21,7 +37,7 @@
 					data:{
 						'id':$('#join').attr('val')
 						},
-					datype: 'json',
+					datatype: 'json',
 					success: function(data){
 						if(data.code != -1){
 							alert(data.msg);
@@ -31,6 +47,27 @@
 					}
 				})
 			})
+			$code = {{$code}};
+			if($code == 233){
+				$timer = setInterval(function(){
+					$.ajax({
+						url:'/front/grab/countdown',
+						data:{
+							id:{{$res[0]->id}}
+						},
+						type:'post',
+						datatype:'json',
+						success:function(data){
+							var html = '';
+							html += '<tr><th>中奖名单</th></tr>';
+							console.log(data.lucky);
+							if(data.code = 200){
+								clearInterval(timer);
+							}
+						}
+					})
+				},1000)
+			}
 		</script>
 	</body>
 </html>
