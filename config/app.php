42135b7c17722bd2aafa8d08c6bbcd339af843af
zxj95121 <986773071@qq.com>
2017-07-31 22:00:18 +0800

    安装image扩展

--- config/app.php
+++ config/app.php
@@ -162,6 +162,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -229,6 +230,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'QrCode' => SimpleSoftwareIO\QrCode\Facades\QrCode::class,
         'Wechat' => App\Http\Libraries\Wechat::class,
+        'Image' => Intervention\Image\Facades\Image::class,
     ],
 
 ];
