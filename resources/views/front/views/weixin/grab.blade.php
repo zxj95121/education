02c404538760f2f42aaebf1288b942d9260547d1
nagnahz <15075138688@163.com>
2017-08-03 10:11:40 +0800

    阿斯达

--- resources/views/front/views/weixin/grab.blade.php
+++ resources/views/front/views/weixin/grab.blade.php
@@ -103,7 +103,7 @@
 						if(data.code != -1){
 							alert(data.msg);
 						}else{
-							console.log(123);
+							console.log('还没填写手机号');
 							$('#dis').trigger("click");
 							return false;
 							//alert('还没填写手机号');
