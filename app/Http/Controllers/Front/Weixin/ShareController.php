0df8cba621d38b2bb7da0f1b0b5020e8b6d84175
zxj95121 <986773071@qq.com>
2017-08-03 23:17:03 +0800

    no message

--- app/Http/Controllers/Front/Weixin/ShareController.php
+++ app/Http/Controllers/Front/Weixin/ShareController.php
@@ -51,10 +51,14 @@ class ShareController extends Controller
 			->select('id', 'name')
 			->get()[0];
 
-		$buyCount = HalfBuyRecord::where('uid', $uid)
-			->where('status', 1)
-			->select('count(record_num) as num')
-			->get()[0]->num;
+		if (HalfBuyRecord::where('uid', $uid)->count > 0) {
+			$buyCount = HalfBuyRecord::where('uid', $uid)
+				->where('status', 1)
+				->select('count(record_num) as num')
+				->get()[0]->num;
+		} else {
+			$buyCount = 0;
+		}
 
 		$newuser = NewUser::where('openid',$openid)->get();
 		if (count($newuser) > 0) {
