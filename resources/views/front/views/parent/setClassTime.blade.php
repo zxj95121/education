b57b4008b34f58501c4fa807cada2ed19946849d
zxj95121 <986773071@qq.com>
2017-08-04 15:17:02 +0800

    no message

--- resources/views/front/views/parent/setClassTime.blade.php
+++ resources/views/front/views/parent/setClassTime.blade.php
@@ -538,9 +538,12 @@
 			})
 
 
-
+			var u = navigator.userAgent;
+		    var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
+		    var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
 			$('#request').focus(function(){
-				$('#request')[0].scrollIntoView(true);
+				if (isiOS)
+					$('#request')[0].scrollIntoView(true);
 			})
 		})
 	</script>
