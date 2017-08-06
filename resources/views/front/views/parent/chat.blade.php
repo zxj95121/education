9139bc8150327c4e1248ad91437dfae8efdd4be0
zxj95121 <986773071@qq.com>
2017-08-06 14:50:02 +0800

    no message

--- resources/views/front/views/parent/chat.blade.php
+++ resources/views/front/views/parent/chat.blade.php
@@ -112,7 +112,7 @@ function emoji_decode($str){
 			       
 			        <div id="sendmessage">
 			        	<!-- <input type="text" id="textInput" style="line-height: 25px;margin-top: 12px;color: #000;" value="" placeholder="" /> -->
-			        	<textarea id="textInput" style="height: 41px;padding-top:3px;margin-top: 4px;padding-left: 0.3em;px;color: #000;line-height: 19px;resize: none;border: 0px solid transparent;font-size: 19px;"></textarea>
+			        	<textarea id="textInput" style="word-wrap : break-word;height: 41px;padding-top:3px;margin-top: 4px;padding-left: 0.3em;px;color: #000;line-height: 19px;resize: none;border: 0px solid transparent;font-size: 19px;"></textarea>
 			          	<input type="file" id="fileInput" style="display: none;" name="file" accept="image/*">
 			            <button id="sendBtn" style="display: none;"><img src="/images/square-send.png" style="width: 100%;height: 100%;cursor: pointer;"></button>
 			            <button id="imageBtn" onclick="fileClick();"><img src="/images/square-image.png" style="width: 100%;height: 100%;cursor: pointer;"></button>
