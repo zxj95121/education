90c833c98f5b0bdb96e52a40148bfda71d626a1d
nagnahz <15075138688@163.com>
2017-08-09 17:24:42 +0800

    测试

--- resources/views/front/views/weixin/classFree.blade.php
+++ resources/views/front/views/weixin/classFree.blade.php
@@ -42,7 +42,7 @@
 					                                    <section class="_135editor" data-tools="135编辑器" data-id="86323" style="border: 0px none; padding: 0px; position: relative; box-sizing: border-box;">
 					                                        <section style="margin-top: 0.5em; margin-bottom: 0.5em; padding-left: 0.5em; padding-right: 0.5em; text-align: center; position: static; box-sizing: border-box;">
 					                                            <section style="position: static;box-sizing: border-box;transform: rotate(0.1deg);-webkit-transform: rotate(0.1deg);-moz-transform: rotate(0.1deg);-ms-transform: rotate(0.1deg);-o-transform: rotate(0.1deg);">
-					                                                <img style="width: 2.2em; margin-bottom: -1.4em; vertical-align: middle;" src="http://image3.135editor.com/cache/remote/aHR0cHM6Ly9tbWJpei5xbG9nby5jbi9tbWJpei9jWlYyaFJwdUFQaWFpY211RWlidzVTS1BrSGxQVUZuVFY1Mm41RENRVTlNYWlhazJCckg5a3hxeVNHbXFDOHJPQk9pYzZLdU1NSHBQelpUaWNmeE9pYXlqRWliREFnLzA="/>
+					                                                <img style="width: 2.2em; margin-bottom: -1.4em; vertical-align: middle;" src="/admin/images/kefu.jpg"/>
 					                                            </section>
 					                                            <section class="layout" style="padding: 7%; box-shadow: rgb(204, 204, 204) 0.3em 0.3em 0.3em; width: 90%; margin-right: auto; margin-left: auto; box-sizing: border-box; background-color: rgb(239, 239, 239);" data-width="90%">
 					                                                <section class="135brush">
