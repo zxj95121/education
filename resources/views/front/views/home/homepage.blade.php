80afefbfcaebe9bee117c30b5bc2d34313428c12
zxj95121 <986773071@qq.com>
2017-08-12 12:59:00 +0800

    no message

--- resources/views/front/views/home/homepage.blade.php
+++ resources/views/front/views/home/homepage.blade.php
@@ -438,10 +438,10 @@ $signPackage = $jssdk->GetSignPackage();
 					},
 					success: function(data) {
 						if (data.errcode == 0) {
-							cartOrder = eval('('+data.order+')');
+							cartOrder = JSON.parse(data.order);
 							cartArr = eval('('+data.arr+')');
 							cartTotal = parseInt(data.total);
-							
+
 							setCartPosition();
             				cartInit();
 						} else {
