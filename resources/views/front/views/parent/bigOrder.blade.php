ea25fd5c8f570f992b12ee0c8ab2ec558cfdfa5e
zxj95121 <986773071@qq.com>
2017-08-17 19:24:07 +0800

    no message

--- resources/views/front/views/parent/bigOrder.blade.php
+++ resources/views/front/views/parent/bigOrder.blade.php
@@ -140,7 +140,7 @@
 					WeixinJSBridge.log(res.err_msg);
 					if(res.err_msg == "get_brand_wcpay_request:ok"){
 						$('#order_pay').replaceWith('<a href="#" class="button button-big button-fill button-danger">已完成</a>');
-						window.location.href="/front/home";  
+						window.location.href="/front/parent/myClassOrder?action=2";  
 					}else{
 					}  
 				}
