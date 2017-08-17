1c9f22433e71179c8ff5c9a2f7beed7ea97ac143
zxj95121 <986773071@qq.com>
2017-08-17 18:17:22 +0800

    no message

--- resources/views/front/views/parent/myClassOrder.blade.php
+++ resources/views/front/views/parent/myClassOrder.blade.php
@@ -5,6 +5,12 @@
 	<meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0">
 	<meta name="csrf-token" content="{{csrf_token()}}">
 	<link rel="stylesheet" type="text/css" href="/css/sm.min.css">
+	<style type="text/css">
+		#orderdetail{
+			width: 96%;
+			margin: 0 auto;
+		}
+	</style>
 </head>
 <body>
 	<div class="page">
