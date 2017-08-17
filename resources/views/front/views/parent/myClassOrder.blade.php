c009b5d8a407feef6c5f9ef0c816c7d44281b24f
zxj95121 <986773071@qq.com>
2017-08-17 18:23:20 +0800

    no message

--- resources/views/front/views/parent/myClassOrder.blade.php
+++ resources/views/front/views/parent/myClassOrder.blade.php
@@ -4,6 +4,7 @@
 	<title>加辰教育</title>
 	<meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0">
 	<meta name="csrf-token" content="{{csrf_token()}}">
+	<link rel="stylesheet" type="text/css" href="/css/weui.css"/>
 	<link rel="stylesheet" type="text/css" href="/css/sm.min.css">
 	<style type="text/css">
 	</style>
