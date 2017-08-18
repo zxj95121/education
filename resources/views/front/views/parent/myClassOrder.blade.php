db59f6b3111898756c3dfd54287b8dd911a61e02
zxj95121 <986773071@qq.com>
2017-08-18 15:21:49 +0800

    no message

--- resources/views/front/views/parent/myClassOrder.blade.php
+++ resources/views/front/views/parent/myClassOrder.blade.php
@@ -83,7 +83,7 @@
 						          		<div class="item-inner">
 							            	<div class="item-title-row">
 							              		<div class="item-title">状态：<span style="color:#3B833E;">排课中</span></div>
-							              		<div class="item-after">{{$value['created_at']}}</div>
+							              		<div class="item-after">{{$value['created_at']}}</div> 
 							            	</div>
 							            	<div class="item-subtitle">价格：<span style="color:#DE5145;">{{$value['price']}}元</span></div>
 							            	<div class="item-subtitle">课时：<span style="color:#2e7900;">{{$value['count']}}次</span></div>
