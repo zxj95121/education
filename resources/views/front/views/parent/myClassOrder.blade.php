a6f7c84041740612b15b8018fdb4ef83c2ebb6bf
zxj95121 <986773071@qq.com>
2017-08-18 18:21:28 +0800

    no message

--- resources/views/front/views/parent/myClassOrder.blade.php
+++ resources/views/front/views/parent/myClassOrder.blade.php
@@ -60,7 +60,7 @@
 						            			已优惠：<span style="font-size: 15px;color: #343639;">{{$value['voucher_num']*88}}元</span>
 						            		</div>
 						            		<div class="item-text" style="text-align: right;">
-						            			<button class="button button-block showOrderDetail" style="color:#FFF;height: 1.6rem;line-height:1.6rem;background: #0894ec;cursor:pointer;">订单详情</button>
+						            			<button class="button button-block showOrderDetail" style="color:#FFF;height: 1.6rem;line-height:1.6rem;background: #0894ec;cursor:pointer;width: 48%;display: inline-block;">订单详情</button>
 						            		</div>
 						            		<button class="button button-block deleteOrderDetail" style="color:#FFF;height: 1.6rem;line-height:1.6rem;background: #ED2424;cursor:pointer;border-color:#ED2424;width: 48%;display: inline-block;">删除订单</button>
 						          		</div>
@@ -123,7 +123,7 @@
 						            			已优惠：<span style="font-size: 15px;color: #343639;">{{$value['voucher_num']*88}}元</span>
 						            		</div>
 						            		<div class="item-text" style="text-align: right;">
-						            			<button class="button button-block showOrderDetail" style="color:#FFF;height: 1.6rem;line-height:1.6rem;background: #0894ec;cursor:pointer;width: 48%;display: inline-block;">订单详情</button>
+						            			<button class="button button-block showOrderDetail" style="color:#FFF;height: 1.6rem;line-height:1.6rem;background: #0894ec;cursor:pointer;">订单详情</button>
 						            		</div>
 						          		</div>
 						        	</a>
@@ -252,6 +252,7 @@
 
 		$(document).on('click', '.deleteOrderDetail', function(e){
 			var vid = $(this).parents('li').attr('vid');
+			var cdom = $(this).parents('payC');
 
 			$.ajax({
 				headers:{
@@ -265,7 +266,9 @@
 				datatype:'json',
 				success:function(data){
 					if (data.errcode == 0) {
-
+						$.alert('删除成功');
+						cdom.prev().remove();
+						cdom.remove();
 					}
 				}
 			})
