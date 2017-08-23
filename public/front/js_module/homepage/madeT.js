bb649d8d6429df67fca0895754d52df123c1d7c7
zxj95121 <986773071@qq.com>
2017-08-24 04:09:46 +0800

    no message

--- public/front/js_module/homepage/madeT.js
+++ public/front/js_module/homepage/madeT.js
@@ -25,10 +25,10 @@ $('#subjectMade').click(function(){
 	      bottom:newage-bottom-position,//新页面底部位置
 	      width:newpage-width,//新页面宽度，默认为100%
 	      height:newpage-height,//新页面高度，默认为100%
-	      ......
+	      // ......
 	    },
 	    extras:{
-	      .....//自定义扩展参数，可以用来处理页面间传值
+	      // .....//自定义扩展参数，可以用来处理页面间传值
 	    },
 	    createNew:false,//是否重复创建同样id的webview，默认为false:不重复创建，直接显示
 	    show:{
@@ -44,7 +44,6 @@ $('#subjectMade').click(function(){
 	      options:{
 	        width:waiting-dialog-widht,//等待框背景区域宽度，默认根据内容自动计算合适宽度
 	        height:waiting-dialog-height,//等待框背景区域高度，默认根据内容自动计算合适高度
-	        ......
 	      }
 	    }
 	})
