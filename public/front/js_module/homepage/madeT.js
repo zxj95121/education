b799f3f6b99c1247514a6498c16d1700ebb63729
zxj95121 <986773071@qq.com>
2017-08-24 05:30:50 +0800

    no message

--- public/front/js_module/homepage/madeT.js
+++ public/front/js_module/homepage/madeT.js
@@ -32,5 +32,14 @@ $('#subjectMade').click(function(){
 $('.selectMade').click(function(){
 	$(this).hide();
 	$(this).next().show();
-	$(this).next()[0].focus();
+	open($(this).next());
 })
+function open(elem) {
+   if (document.createEvent) {
+       var e = document.createEvent("MouseEvents");
+       e.initMouseEvent("mousedown", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+       elem[0].dispatchEvent(e);
+   } else if (element.fireEvent) {
+       elem[0].fireEvent("onmousedown");
+   }
+}
