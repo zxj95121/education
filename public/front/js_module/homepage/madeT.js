f74f5b664c90b9f747ab9a49c8deeb0f7ea37089
zxj95121 <986773071@qq.com>
2017-08-24 03:46:24 +0800

    no message

--- public/front/js_module/homepage/madeT.js
+++ public/front/js_module/homepage/madeT.js
@@ -4,12 +4,12 @@ $('#madeT_ul li').click(function(){
 	$(href).show();
 })
 
-$('#segmentedControl .mui-control-item').click(function(){
-	$('#segmentedControl .mui-control-item').each(function(){
-		$(this).removeClass('mui-active');
-	})
+$('#segmentedControl .mui-control-item').touchstart(function(){
+	// $('#segmentedControl .mui-control-item').each(function(){
+	// 	$(this).removeClass('mui-active');
+	// })
 
-	$(this).addClass('mui-active');
+	// $(this).addClass('mui-active');
 	var href = $(this).attr('for');
 	$('.madeSteps').hide();
 	$(href).show();
