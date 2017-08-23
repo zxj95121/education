383e7f796132826625b70326fccbfcf9bac54421
zxj95121 <986773071@qq.com>
2017-08-24 03:27:11 +0800

    no message

--- resources/views/front/views/home/homepage2.blade.php
+++ resources/views/front/views/home/homepage2.blade.php
@@ -57,9 +57,9 @@ $signPackage = $jssdk->GetSignPackage();
 		@if($parentDetail->type == 1)
 		<div class="container-fluid mui-control-content" id="teacher">
 			@if($parentDetail->id == 21)
-			<div class="mui-col-xs-12 mui-col-sm-12 madeT_Div" id="madeT_ul">		
+			<div class="mui-col-xs-12 mui-col-sm-12 madeT_Div">		
 
-				<ul class="mui-table-view mui-grid-view mui-grid-9">
+				<ul class="mui-table-view mui-grid-view mui-grid-9" id="madeT_ul">
 		            <li class="mui-table-view-cell mui-media mui-col-xs-6 mui-col-sm-6" hr="#madeT_made"><a href="#">
 		                    <span class="mui-icon mui-icon-compose"></span>
 		                    <div class="mui-media-body">教师定制</div></a></li>
