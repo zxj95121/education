af036bbde8be6e9992039d395afb9657bfbf712f
zxj95121 <986773071@qq.com>
2017-08-24 01:00:06 +0800

    .

--- resources/views/front/views/home/homepage2.blade.php
+++ resources/views/front/views/home/homepage2.blade.php
@@ -65,7 +65,126 @@ $signPackage = $jssdk->GetSignPackage();
 	                    <span class="mui-icon mui-icon-paperclip"><span class="mui-badge">5</span></span>
 	                    <div class="mui-media-body">定制历史</div></a></li>
 	        </ul>
-	       
+	       <div class="mui-content">
+				<div id="slider" class="mui-slider" data-slider="4">
+					<div id="sliderSegmentedControl" class="mui-slider-indicator mui-segmented-control mui-segmented-control-inverted">
+						<a class="mui-control-item" href="#item1mobile">
+					待办公文
+				</a>
+						<a class="mui-control-item mui-active" href="#item2mobile">
+					已办公文
+				</a>
+						<a class="mui-control-item" href="#item3mobile">
+					全部公文
+				</a>
+					</div>
+					<div id="sliderProgressBar" class="mui-slider-progress-bar mui-col-xs-4" style="transform: translate3d(280px, 0px, 0px) translateZ(0px);"></div>
+					<div class="mui-slider-group" style="transform: translate3d(-841px, 0px, 0px) translateZ(0px); transition-duration: 0ms; transition-timing-function: cubic-bezier(0.165, 0.84, 0.44, 1);">
+						<div id="item1mobile" class="mui-slider-item mui-control-content">
+							<div id="scroll1" class="mui-scroll-wrapper" data-scroll="1">
+								<div class="mui-scroll" style="transform: translate3d(0px, 0px, 0px) translateZ(0px); transition-duration: 0ms;">
+									<ul class="mui-table-view">
+										<li class="mui-table-view-cell">
+											第一个选项卡子项-1
+										</li>
+										<li class="mui-table-view-cell">
+											第一个选项卡子项-2
+										</li>
+										<li class="mui-table-view-cell">
+											第一个选项卡子项-3
+										</li>
+										<li class="mui-table-view-cell">
+											第一个选项卡子项-4
+										</li>
+										<li class="mui-table-view-cell">
+											第一个选项卡子项-5
+										</li>
+										<li class="mui-table-view-cell">
+											第一个选项卡子项-6
+										</li>
+										<li class="mui-table-view-cell">
+											第一个选项卡子项-7
+										</li>
+										<li class="mui-table-view-cell">
+											第一个选项卡子项-8
+										</li>
+										<li class="mui-table-view-cell">
+											第一个选项卡子项-9
+										</li>
+										<li class="mui-table-view-cell">
+											第一个选项卡子项-10
+										</li>
+										<li class="mui-table-view-cell">
+											第一个选项卡子项-11
+										</li>
+										<li class="mui-table-view-cell">
+											第一个选项卡子项-12
+										</li>
+										<li class="mui-table-view-cell">
+											第一个选项卡子项-13
+										</li>
+										<li class="mui-table-view-cell">
+											第一个选项卡子项-14
+										</li>
+										<li class="mui-table-view-cell">
+											第一个选项卡子项-15
+										</li>
+										<li class="mui-table-view-cell">
+											第一个选项卡子项-16
+										</li>
+										<li class="mui-table-view-cell">
+											第一个选项卡子项-17
+										</li>
+										<li class="mui-table-view-cell">
+											第一个选项卡子项-18
+										</li>
+										<li class="mui-table-view-cell">
+											第一个选项卡子项-19
+										</li>
+										<li class="mui-table-view-cell">
+											第一个选项卡子项-20
+										</li>
+									</ul>
+								</div>
+							<div class="mui-scrollbar mui-scrollbar-vertical"><div class="mui-scrollbar-indicator" style="transition-duration: 0ms; display: block; height: 52px; transform: translate3d(0px, 0px, 0px) translateZ(0px);"></div></div></div>
+						</div>
+						<div id="item2mobile" class="mui-slider-item mui-control-content mui-active">
+							<div id="scroll2" class="mui-scroll-wrapper" data-scroll="2">
+								<div class="mui-scroll" style="transform: translate3d(0px, 0px, 0px) translateZ(0px); transition-duration: 0ms;"><ul class="mui-table-view"><li class="mui-table-view-cell">第二个选项卡子项-1</li><li class="mui-table-view-cell">第二个选项卡子项-2</li><li class="mui-table-view-cell">第二个选项卡子项-3</li><li class="mui-table-view-cell">第二个选项卡子项-4</li><li class="mui-table-view-cell">第二个选项卡子项-5</li></ul></div>
+							<div class="mui-scrollbar mui-scrollbar-vertical"><div class="mui-scrollbar-indicator" style="transition-duration: 0ms; display: block; height: 207px; transform: translate3d(0px, 0px, 0px) translateZ(0px);"></div></div></div>
+
+						</div>
+						<div id="item3mobile" class="mui-slider-item mui-control-content">
+							<div id="scroll3" class="mui-scroll-wrapper" data-scroll="3">
+								<div class="mui-scroll" style="transform: translate3d(0px, -1px, 0px) translateZ(0px); transition-duration: 0ms;"><ul class="mui-table-view"><li class="mui-table-view-cell">第三个选项卡子项-1</li><li class="mui-table-view-cell">第三个选项卡子项-2</li><li class="mui-table-view-cell">第三个选项卡子项-3</li><li class="mui-table-view-cell">第三个选项卡子项-4</li><li class="mui-table-view-cell">第三个选项卡子项-5</li></ul></div>
+							<div class="mui-scrollbar mui-scrollbar-vertical" style="transition-duration: 500ms; opacity: 0;"><div class="mui-scrollbar-indicator" style="transition-duration: 0ms; display: block; height: 207px; transform: translate3d(0px, 2px, 0px) translateZ(0px);"></div></div></div>
+
+						</div>
+					</div>
+				</div>
+
+				<h5 class="mui-content-padded">Color</h5>
+				<div class="mui-card">
+					<form class="mui-input-group">
+						<div class="mui-input-row mui-radio">
+							<label><span class="mui-bg-primary" style="display:block;width:25px;height:25px;"></span>
+							</label>
+							<input name="radio1" type="radio" checked="" value="primary">
+						</div>
+						<div class="mui-input-row mui-radio">
+							<label><span class="mui-bg-positive" style="display:block;width:25px;height:25px;"></span>
+							</label>
+							<input name="radio1" type="radio" value="positive">
+						</div>
+						<div class="mui-input-row mui-radio">
+							<label><span class="mui-bg-negative" style="display:block;width:25px;height:25px;"></span>
+							</label>
+							<input name="radio1" type="radio" value="negative">
+						</div>
+					</form>
+				</div>
+
+			</div>
 	        <div class="mui-col-xs-12 mui-col-sm-12 madeT_Div" id="madeT_history">
 	        	
 	        </div>
@@ -438,10 +557,47 @@ $signPackage = $jssdk->GetSignPackage();
   //   	mui.init({
 		// 	// swipeBack:true //启用右滑关闭功能
 		// });
-		mui.init({
-			swipeBack: true
-		});
+		// mui.init({
+		// 	swipeBack: true
+		// });
     </script>
+    <script>
+			mui.init({
+				swipeBack: false
+			});
+			(function($) {
+				$('.mui-scroll-wrapper').scroll({
+					indicators: true //是否显示滚动条
+				});
+				var html2 = '<ul class="mui-table-view"><li class="mui-table-view-cell">第二个选项卡子项-1</li><li class="mui-table-view-cell">第二个选项卡子项-2</li><li class="mui-table-view-cell">第二个选项卡子项-3</li><li class="mui-table-view-cell">第二个选项卡子项-4</li><li class="mui-table-view-cell">第二个选项卡子项-5</li></ul>';
+				var html3 = '<ul class="mui-table-view"><li class="mui-table-view-cell">第三个选项卡子项-1</li><li class="mui-table-view-cell">第三个选项卡子项-2</li><li class="mui-table-view-cell">第三个选项卡子项-3</li><li class="mui-table-view-cell">第三个选项卡子项-4</li><li class="mui-table-view-cell">第三个选项卡子项-5</li></ul>';
+				var item2 = document.getElementById('item2mobile');
+				var item3 = document.getElementById('item3mobile');
+				document.getElementById('slider').addEventListener('slide', function(e) {
+					if (e.detail.slideNumber === 1) {
+						if (item2.querySelector('.mui-loading')) {
+							setTimeout(function() {
+								item2.querySelector('.mui-scroll').innerHTML = html2;
+							}, 500);
+						}
+					} else if (e.detail.slideNumber === 2) {
+						if (item3.querySelector('.mui-loading')) {
+							setTimeout(function() {
+								item3.querySelector('.mui-scroll').innerHTML = html3;
+							}, 500);
+						}
+					}
+				});
+				var sliderSegmentedControl = document.getElementById('sliderSegmentedControl');
+				$('.mui-input-group').on('change', 'input', function() {
+					if (this.checked) {
+						sliderSegmentedControl.className = 'mui-slider-indicator mui-segmented-control mui-segmented-control-inverted mui-segmented-control-' + this.value;
+						//force repaint
+						sliderProgressBar.setAttribute('style', sliderProgressBar.getAttribute('style'));
+					}
+				});
+			})(mui);
+		</script>
     <script type="text/javascript">
     	$(function(){
     		newUserId = '{{$newUserId}}';
