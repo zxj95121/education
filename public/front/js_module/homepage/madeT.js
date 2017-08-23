2188e28a7842d50b4dc28b4eb574c681203768b0
zxj95121 <986773071@qq.com>
2017-08-24 05:38:28 +0800

    no message

--- public/front/js_module/homepage/madeT.js
+++ public/front/js_module/homepage/madeT.js
@@ -37,9 +37,9 @@ $('.selectMade').click(function(){
 function open(elem) {
    if (document.createEvent) {
        var e = document.createEvent("MouseEvents");
-       e.initMouseEvent("mousedown", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+       e.initMouseEvent("touchstart", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
        elem[0].dispatchEvent(e);
    } else if (element.fireEvent) {
-       elem[0].fireEvent("onmousedown");
+       elem[0].fireEvent("ontouchstart");
    }
 }
