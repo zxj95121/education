738fb5b96db6727a234eaa2e111cd1dab950ec22
zxj95121 <986773071@qq.com>
2017-08-24 18:25:24 +0800

    no message

--- resources/views/front/views/home/homepage2.blade.php
+++ resources/views/front/views/home/homepage2.blade.php
@@ -15,6 +15,7 @@ $signPackage = $jssdk->GetSignPackage();
     <link rel="stylesheet" type="text/css" href="/css/weui.css"/>
     <link rel="stylesheet" type="text/css" href="/front/css_module/homepage/my.css">
     @if($userType->type == 2 && $parentDetail->id == 21)
+    <link rel="stylesheet" type="text/css" href="/js/mui/plugin/picker/dist/css/mui.picker.min.css"">
     <link rel="stylesheet" type="text/css" href="/front/css_module/homepage/madeT.css">
     @else
     @endif
