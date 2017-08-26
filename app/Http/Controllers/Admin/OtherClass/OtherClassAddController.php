e09255b0a1690cc033513eefc683cef85e29146d
zxj95121 <986773071@qq.com>
2017-08-26 19:42:17 +0800

    no message

--- app/Http/Controllers/Admin/OtherClass/OtherClassAddController.php
+++ app/Http/Controllers/Admin/OtherClass/OtherClassAddController.php
@@ -98,8 +98,9 @@ class OtherClassAddController extends Controller
             ->where('class_package_order.status', 1)
             ->leftJoin('new_user as nu', 'nu.id', 'class_package_order.uid')
             ->select('nu.nickname', 'nu.phone as phone', 'class_package_order.*' ,'nu.paty as patynum','nu.id as userId')
+            ->orderBy('class_package_order.created_at', 'desc')
             ->paginate(10);
-        dd($orderObj->toArray());
+        // dd($orderObj->toArray());
         return view('admin.otherClass.orderList', ['package'=>$packageObj,'orderObj'=>$orderObj,'cid'=>$cid]);
     }
 }
