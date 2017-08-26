f185c07275d7ddedbb7572c68c24918c86b1f4b6
zxj95121 <986773071@qq.com>
2017-08-26 19:03:04 +0800

    no message

--- app/Http/Controllers/Admin/MessageRecordController.php
+++ app/Http/Controllers/Admin/MessageRecordController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 use App\Models\ModifyPriceRecord;
+use App\Models\BigOrder;
+use App\Models\ClassPackageOrder;
 
 class MessageRecordController extends Controller
 {
@@ -14,10 +16,20 @@ class MessageRecordController extends Controller
     	$recordObj = ModifyPriceRecord::where('modify_price_record.status', 1)
     		->leftJoin('new_user as nu', 'nu.id', 'modify_price_record.uid')
     		->leftJoin('admin_info as ai', 'ai.openid', 'nu.openid')
-    		->leftJoin('big_order as bo', 'bo.id', 'modify_price_record.order_no')
     		->orderBy('modify_price_record.id', 'desc')
-    		->select('ai.name','bo.order_no as no','modify_price_record.*')
+    		->select('ai.name','modify_price_record.*')
     		->paginate(10);
+
+    	foreach($recordObj as $value) {
+    		if ($value->which == 1) {
+    			$orderno = BigOrder::find($value->order_no)->order_no;
+    		} elseif ($value->which == 2) {
+    			$orderno = ClassPackageOrder::find($value->order_no)->order_no;
+    		} else {
+    			$orderno = '';
+    		}
+  			$recordObj->no = $orderno;
+    	}
     	return view('admin.record.modifyPrice', ['recordObj'=>$recordObj]);
     }
 }
