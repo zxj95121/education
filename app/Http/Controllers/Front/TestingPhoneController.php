6831678a46187e40eb290011f658fe3d548e08d6
zxj95121 <986773071@qq.com>
2017-08-27 17:42:09 +0800

    no message

--- app/Http/Controllers/Front/TestingPhoneController.php
+++ app/Http/Controllers/Front/TestingPhoneController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Front;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\NewUser;
+use App\Models\ClassFree;
 use Session;
 use Wechat;
 class TestingPhoneController extends Controller
