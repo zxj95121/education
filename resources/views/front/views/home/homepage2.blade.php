025b2ce208eda31e76103751ea78604e1a5998e3
zxj95121 <986773071@qq.com>
2017-08-29 13:20:25 +0800

    no message

--- resources/views/front/views/home/homepage2.blade.php
+++ resources/views/front/views/home/homepage2.blade.php
@@ -83,7 +83,7 @@ $signPackage = $jssdk->GetSignPackage();
 					</div>
 				</div> -->
 				<div class="mui-content" style="position: relative;">
-					<span onclick="initForm();" style="display: block;width:100%;text-align:right;font-size:0.89rem;padding-right: 10px;padding-top: 7px;cursor: pointer;">初始化定制<i class="glyphicon glyphicon-erase"></i></span>
+					<span onclick="initForm();" style="display: block;width:100%;text-align:right;font-size:1rem;padding-right: 10px;padding-top: 7px;cursor: pointer;">初始化定制<i class="glyphicon glyphicon-erase"></i></span>
 					<div class="mui-col-xs-12 mui-col-sm-12 madeSteps" id="directionMade">
 						<div style="padding: 10px 10px;">
 							<form class="mui-input-group">
